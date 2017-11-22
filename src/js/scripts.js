f2af1689dacdc574719d447834891983bc9a506c
Kristen Kriens <kristen.kriens@gmail.com>
2017-11-22 14:37:28 -0500

    Remove map overlay

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -18,7 +18,7 @@ app.eventApiKey = 'srQBgzwWJzXwZcrM';
 
 // Generates the base map for the app
 app.generateMap = function() {
-  const mapContainer = $('.map__map')[0];
+  const mapContainer = $('.map')[0];
 
   const mapStyle = [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#a0d683"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#b2e2f0"}]}];
 
@@ -200,7 +200,7 @@ app.drawDistanceRadius = function() {
 	app.map.fitBounds(app.distanceRadius.getBounds());
 }
 
-// Generates event markers on map and fills out number of results in overlay
+// Generates event markers on map
 app.generateEvents = function(events) {
 	let event = events.events.event;
 	console.log(event)
@@ -220,9 +220,6 @@ app.generateEvents = function(events) {
 
 		eventMarker.addListener('click', app.showAbout);
   }
-
-	let totalItems = event.length;
-  $('.map__results-number').text(totalItems);
 }
 
 // Generates the event marker symbol
